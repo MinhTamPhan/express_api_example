4314ae192bc97a629387e107483b17d661d7f3f8
tampm2 <tampm2@vng.com.vn>
2020-07-12 22:18:28 +0700

    add config .env

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.listen(process.env.EXPOSE_PORT, () => {
+  console.log(`API running on PORT ${process.env.EXPOSE_PORT}`)
+})
